ef65e38d7ffa95615c28ccfd835929554568b27d
Sarah Murray <sarahmurraydev@gmail.com>
2020-01-04 01:16:20 -0500

    added a login component for ui auth

--- playlist-ui/src/Login.js
+++ playlist-ui/src/Login.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { API_URL } from './constants'
+// import { API_URL } from './constants'
+import { connect } from 'react-redux'
+import {
+    getUserData
+} from './actions/actionCreator';
 
 
 class Login extends React.Component{
     render(){
+
+        const {
+            getProfile
+        } = this.props;
+
         return <div id='login'>
             <h1>This is an example of the Authorization Code flow</h1>
-            <a href={`${API_URL}/api/login`} className="btn btn-primary">Log in with Spotify</a>
+            <a href="http://localhost:3001/api/login" className="btn btn-primary">Log in with Spotify</a>
         </div>
     }
 }
 
-export default Login
+const mapStateToProps = state => {
+    return {
+        userData: state.userData
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getProfile: () => dispatch(getUserData)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
